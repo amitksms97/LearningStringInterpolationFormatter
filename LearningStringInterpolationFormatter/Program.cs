4c390824ef1ef7984d393b1f21f0ad7566af5356
amitksms97 <amitksms97@gmail.com>
2021-05-01 20:12:05 +0530

    Add project files.

---
+++ LearningStringInterpolationFormatter/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LearningStringInterpolationFormatter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var point = new { x = 1.1234, y = 2.1254 };
+            Console.WriteLine($"x {point.x} y{point.y,10:N1}");
+        }
+    }
+}
